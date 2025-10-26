658d3fde79115be948e92f8ee7bbf0ec26ef07ec
tuckriver <mononoke1997@gmail.com>
2025-10-26 22:59:04 +0900

    新規登録・ログインの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname_kanji, :firstname_kanji, :lastname_katakana, :firstname_katakana, :birth_date])
+  end  
 end
