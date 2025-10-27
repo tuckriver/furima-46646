eb9cd4730823451c98a7637051847e9d9f729ead
tuckriver <mononoke1997@gmail.com>
2025-10-27 15:38:31 +0900

    ActiveHashの実装
    ActiveHashの導入
    ビューファイルへの反映

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ApplicationRecord
   belongs_to :user
 
   has_one_attached :product_image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :delivery_time
+  belongs_to :delivery_cost
+  belongs_to :product_status
+  belongs_to :category
+
+  #空の投稿を保存できないようにする
+  validates :prefecture_id, :delivery_time_id, :delivery_cost_id, :product_status_id, :category_id, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :prefecture_id, :delivery_time_id, :delivery_cost_id, :product_status_id, :category_id, numericality: { other_than: 1, message: "can't be blank"} 
 end
