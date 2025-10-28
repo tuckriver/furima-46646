a7f0f5dd2315e8200ff8901bfbfd815ef4a8f1a8
tuckriver <mononoke1997@gmail.com>
2025-10-28 21:30:27 +0900

    ビュー及びコントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -35,6 +35,11 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+  
   private
 
   def product_params
