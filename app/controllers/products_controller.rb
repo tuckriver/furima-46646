0a602597db137317dbc3340c2bf4b9fa3df825dc
tuckriver <mononoke1997@gmail.com>
2025-10-28 19:48:20 +0900

    ビュー及びコントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -21,6 +21,23 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def edit
+    @product = Product.find(params[:id])
+    unless user_signed_in? && @product.user_id == current_user.id
+           redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+       redirect_to product_path(@product.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
