cce43bc293dd9ff15aa5464b2f4cb69ae8bda49a
tuckriver <mononoke1997@gmail.com>
2025-10-28 10:51:38 +0900

    単体テストコード

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,9 +11,11 @@ class Product < ApplicationRecord
   belongs_to :product_status
   belongs_to :category
 
-  #空の投稿を保存できないようにする
-  validates :prefecture_id, :delivery_time_id, :delivery_cost_id, :product_status_id, :category_id, :product_name, :description, :product_price, :product_image, presence: true
+  
+  validates :product_name, :description, :product_price, :product_image, presence: true
 
-  #ジャンルの選択が「---」の時は保存できないようにする
   validates :prefecture_id, :delivery_time_id, :delivery_cost_id, :product_status_id, :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+
+  validates :product_price,format: { with: /\A[0-9]+\z/ },
+                           numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name        {Faker::Commerce.product_name} 
+    description         {Faker::Lorem.sentence}
+    product_status_id   {2}
+    category_id         {2}
+    prefecture_id       {2}
+    delivery_time_id    {2}
+    delivery_cost_id    {2}
+    product_price       {Faker::Number.between(from: 300, to: 9_999_999)}
+
+
+    association :user
+
+
+    after(:build) do |product|
+      product.product_image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
